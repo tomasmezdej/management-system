d5c86704d7b439c113a9db00bdc74a105e499165
Tomáš Meždej <tmezdej@gmail.com>
2023-02-22 18:51:36 +0100

    navbar title + responsivity

--- src/components/navbar/NavBarTitle.jsx
+++ src/components/navbar/NavBarTitle.jsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, WrapItem, Avatar } from "@chakra-ui/react"
 import { GiHamburgerMenu } from "react-icons/gi"
 
 /* import { modeCard } from "../../store/actions/modeCard";
@@ -23,16 +23,38 @@ const NavBarTitle = (props) => {/*
     <Flex
       className="navbar__title"
       style={props.style}
-      justify="space-between"
     >
-      <div>
-        im title
-      </div>
-      <GiHamburgerMenu
-        className="navbar__hamburger"
-        size={30}
-        onClick={handleHamburgerClick}
-      />
+      <WrapItem className="navbar__title_avatar_mobile" w="100%" alignItems="center" gap={2}>
+        <Avatar size='sm' name='Christian Nwamba' src='https://bit.ly/code-beast' />{' '}
+        <h1 className="">
+          Alex Johnson
+        </h1>
+      </WrapItem>
+      <Flex
+        className="navbar__title_avatar_mobile_hidden"
+        w="100%"
+        alignItems="center"
+        justifyContent="center"
+        bg="orange"
+        direction="column"
+      >
+        <WrapItem className="navbar__title_avatar">
+          <Avatar size='xl' name='Christian Nwamba' src='https://bit.ly/code-beast' />{' '}
+        </WrapItem>
+        <h1 className="navbar__title_name">
+          Alex Johnson
+        </h1>
+        <h2 className="navbar__title_email">
+          alex.johnson@gmail.com
+        </h2>
+      </Flex>
+      <WrapItem w="100%" justifyContent="flex-end">
+        <GiHamburgerMenu
+          className="navbar__hamburger"
+          size={30}
+          onClick={handleHamburgerClick}
+        />
+      </WrapItem>
     </Flex>
   )
 }
